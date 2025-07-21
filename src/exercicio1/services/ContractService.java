0d548ee1d590b3fec7a9ba36b024c96bf20d9eeb
Gabriel Avena <gabrielpavena@gmail.com>
2025-07-21 08:41:34 -0300

    exercicio_fixacao_interfaces

---
+++ src/exercicio1/services/ContractService.java
@@ -0,0 +1,28 @@
+package exercicio1.services;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+import exercicio1.entities.Contract;
+import exercicio1.entities.Installment;
+
+public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months) {
+        double basicQuota = contract.getTotalValue() / months;
+        for (int i = 1; i <= months; i++) {
+            LocalDate dueDate = contract.getDate().plusMonths(i);
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + interest + fee;
+            contract.getInstallments().add(new Installment(dueDate, quota));
+        }
+    }
+}
